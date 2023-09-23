d7e63d1ce960abb6a1e055a118c59824cee7c330
Sutheera P <64070112@kmitl.ac.th>
2023-09-24 03:39:01 +0700

    scene 3 update

--- LandingKrabi/content1_sa/script.js
+++ LandingKrabi/content1_sa/script.js
@@ -2,9 +2,6 @@ function delayScroll(next){
     $('html, body').animate({
         scrollTop: $('#'+next).offset().top
       }, 1000);
-}
 
-function scroll(next){
-    console.log('inScroll')
-    document.getElementById(next).scrollIntoView({behavior: "smooth"});
+    //   แค่ delay ทำหน่วงไงว้า
 }
