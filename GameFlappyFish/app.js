bf3740d2221c38db7be092f9aaab7d0a42d4c87b
Adamam <88430865+Adamam13@users.noreply.github.com>
2023-09-25 02:21:02 +0700

    collision check
     collision check in button object

--- GameFlappyFish/app.js
+++ GameFlappyFish/app.js
@@ -30,29 +30,24 @@ function game(){
   let p_b = parseInt(player.getBoundingClientRect().bottom)
   // console.log(p_b)
 
-
+  //collision check
   for (let t of ob_top){
     let t_l = parseInt(t.getBoundingClientRect().left);
     let t_r = parseInt(t.getBoundingClientRect().right);
     let t_b = parseInt(t.getBoundingClientRect().bottom);
-    // console.log(t_l);
-    // console.log(t_r);
-    // console.log(t_b);
-
-    if (p_r >= t_l && p_l <= t_r){
-      // console.log("check1")
-      if (p_t <= t_b){
-        // console.log("check2")
+    if (p_r >= t_l && p_l <= t_r && p_t <= t_b){
         player.style.setProperty('--cor', 'red');
-      }
     }
-
   }
-  // let info_ob = ob.getBoundingClientRect().x;
-  // console.log(info_ob);
 
-  // let info_p = player.getBoundingClientRect().y;
-  // console.log(info_p)
+  for (let b of ob_button){
+    let b_l = parseInt(b.getBoundingClientRect().left);
+    let b_r = parseInt(b.getBoundingClientRect().right);
+    let b_t = parseInt(b.getBoundingClientRect().top);
+    if (p_r >= b_l && p_l <= b_r && p_t <= b_t){
+        player.style.setProperty('--cor', 'red');
+    }
+  }
 }
 
 function swim(){
