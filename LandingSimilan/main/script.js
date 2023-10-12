ddab044e37e02da674b4d34d384d733ddcb5f9b1
64070109Supanat <88423049+JaimesJames@users.noreply.github.com>
2023-10-13 02:19:26 +0700

    test similan

--- LandingSimilan/main/script.js
+++ LandingSimilan/main/script.js
@@ -145,8 +145,6 @@ function nextFishCard() {
   card.innerHTML = html;
   nowFishNumber = nextFishNumber;
 
-
-
   //ขยับด้านหลัง
   nowleft = fishgal.scrollLeft;
   fishgal.scrollLeft = nowleft - (nowleft % fishWidth) + fishWidthGap;
@@ -194,195 +192,199 @@ function scrollY(pageid) {
   scrollToTop = parseInt((pageStat.bottom - 834) * -1 + pos); // + ต่อจาก top เดิม
 
   //call class
-  let text2 = document.querySelector('.textpage2')
-  let text3 = document.querySelector('.textpage3')
-  let text4 = document.querySelector('.textpage4')
-  let text5 = document.querySelector('.textpage5')
-  let text6 = document.querySelector('.textpage6')
+  let text2 = document.querySelector(".textpage2");
+  let text3 = document.querySelector(".textpage3");
+  let text4 = document.querySelector(".textpage4");
+  let text5 = document.querySelector(".textpage5");
+  let text6 = document.querySelector(".textpage6");
   let text1top = 100;
   let text1 = document.querySelector(".t1");
   let coral2 = document.querySelectorAll(".c2");
 
-  let seaweed1_2 = document.querySelector('.seaweedleft_2')
-  let seaweed2_2 = document.querySelector('.seaweedright1_2')
-  let seaweed2_3 = document.querySelector('.seaweedright2_2')
-  let island2 = document.querySelector('.island_2')
-  let islandwave2 = document.querySelector('.islandwave_2')
-  let seaweedback2 = document.querySelector('.seaweedback_2')
-  
+  let seaweed1_2 = document.querySelector(".seaweedleft_2");
+  let seaweed2_2 = document.querySelector(".seaweedright1_2");
+  let seaweed2_3 = document.querySelector(".seaweedright2_2");
+  let island2 = document.querySelector(".island_2");
+  let islandwave2 = document.querySelector(".islandwave_2");
+  let seaweedback2 = document.querySelector(".seaweedback_2");
 
-  let sceneback3 = document.querySelector('.sceneback_3')
-  let shark3 = document.querySelector('.shark_3')
-  let scenemid3 = document.querySelector('.scenemid_3')
-  let scenefront3 = document.querySelector('.scenefront_3')
-  let cfront13 = document.querySelector('.coralfront1_3')
-  let cfront23 = document.querySelector('.coralfront2_3')
-  let cmid = document.querySelector('.coralmid_3')
+  let sceneback3 = document.querySelector(".sceneback_3");
+  let shark3 = document.querySelector(".shark_3");
+  let scenemid3 = document.querySelector(".scenemid_3");
+  let scenefront3 = document.querySelector(".scenefront_3");
+  let cfront13 = document.querySelector(".coralfront1_3");
+  let cfront23 = document.querySelector(".coralfront2_3");
+  let cmid = document.querySelector(".coralmid_3");
 
-  let island4 = document.querySelector('.island_4')
-  let islandwave4 = document.querySelector('.wave_4')
-  let rock4 = document.querySelector('.rock_4')
-  let fg = document.querySelector('.fish-gallery')
+  let island4 = document.querySelector(".island_4");
+  let islandwave4 = document.querySelector(".wave_4");
+  let rock4 = document.querySelector(".rock_4");
+  let fg = document.querySelector(".fish-gallery");
 
-  let rock5 = document.querySelector('.rock_5')
-  let fc1 = document.querySelector('.fc1')
-  let fc2 = document.querySelector('.fc2')
-  let fc3 = document.querySelector('.fc3')
+  let rock5 = document.querySelector(".rock_5");
+  let fc1 = document.querySelector(".fc1");
+  let fc2 = document.querySelector(".fc2");
+  let fc3 = document.querySelector(".fc3");
 
-  let sp1 = document.querySelector('.sp1')
-  let sp2 = document.querySelector('.sp2')
-  let sp3 = document.querySelector('.sp3')
+  let sp1 = document.querySelector(".sp1");
+  let sp2 = document.querySelector(".sp2");
+  let sp3 = document.querySelector(".sp3");
 
-  let rockback6 = document.querySelector('.rockback_6')
-  let rockmid6 = document.querySelector('.rockmid_6')
-  let cfront6 = document.querySelector('.coralfront_6')
-  let trash1 = document.querySelector('.trash1_6')
-  let trash2 = document.querySelector('.trash2_6')
-  let trash3 = document.querySelector('.trash3_6')
-  
+  let rockback6 = document.querySelector(".rockback_6");
+  let rockmid6 = document.querySelector(".rockmid_6");
+  let cfront6 = document.querySelector(".coralfront_6");
+  let trash1 = document.querySelector(".trash1_6");
+  let trash2 = document.querySelector(".trash2_6");
+  let trash3 = document.querySelector(".trash3_6");
+
   // frame animation
   let id = setInterval(frame, 0.1);
   function frame() {
-    if (pos == scrollToTop || pos == scrollToTop - 1 || pos == scrollToTop - 2 ) {
+    if (
+      pos == scrollToTop ||
+      pos == scrollToTop - 1 ||
+      pos == scrollToTop - 2
+    ) {
       clearInterval(id);
-    }
-    else {
+    } else {
       pos -= 3;
       body.style.top = pos + "px";
-      console.log(pos)
-      if (pos >= -1094) {
-        text1top += pos * -0.003;
-        text1.style.setProperty('opacity',1+pos*0.003)
-        text1.style.setProperty('filter','blur(4px)')
-      }
-      //1
-      text1.style.top = text1top + 'px'
-      
-      //2
-      if (pos <= -600) {
-        seaweed1_2.style.setProperty('top', '33%')
-        seaweed2_2.style.setProperty('top', '18%')
-        seaweed2_3.style.setProperty('top', '26%')
-        island2.style.setProperty('bottom', '0')
-        islandwave2.style.setProperty('bottom', '0')
-        seaweedback2.style.setProperty('top', '15%')
-        text2.style.setProperty('opacity', '1')
-        for (let h of coral2) {
-          h.style.setProperty('bottom', '-1%')
-          // h.style.setProperty('--cb', '6px')
-        }
-      }
-      if(pos <= -500){
-        for (let h of coral2) {
-          h.style.setProperty('--cb', 6+pos*0.005+'px')
-        }
-        seaweedback2.style.setProperty('--cb', 5+pos*0.004+'px')
-        seaweed1_2.style.setProperty('--cb',5+pos*0.004+'px')
-        seaweed2_2.style.setProperty('--cb', 6+pos*0.004+'px')
-        seaweed2_3.style.setProperty('--cb', 4+pos*0.004+'px')
-      }
-      if(pos <= -1100){
-        text2.style.setProperty('filter','blur(20px)')
+      console.log(pos);
+      text1.style.top = text1top + "px";
+      if (pageid == "#page1") {
+        //1
       }
 
-      //3
-      if(pos <= -1200){
-        sceneback3.style.setProperty('bottom', '15%')
-        scenemid3.style.setProperty('bottom', '0%')
-        scenefront3.style.setProperty('bottom', '0%')
-        text3.style.setProperty('margin-top', '15%')
-        text3.style.setProperty('opacity', '1')
-        shark3.style.setProperty('right','-4%')
-        shark3.style.setProperty('top','10%')
-        
-      }
-      if(pos <= -1700){
-        // console.log(1)
-        if( pos >= -2000){
-          cfront13.style.setProperty('--cbb', 10+pos*0.005+'px')
-          cfront23.style.setProperty('--cbb', 10+pos*0.005+'px')
-          cmid.style.setProperty('--cbb', 2+pos*0.005+'px')
+      if (pageid == "#page2") {
+        if (pos >= -1094) {
+          text1top += pos * -0.003;
+          text1.style.setProperty("opacity", 1 + pos * 0.003);
+          text1.style.setProperty("filter", "blur(4px)");
         }
-        
-      }
+        //2
+        if (pos <= -600) {
+          seaweed1_2.style.setProperty("top", "33%");
+          seaweed2_2.style.setProperty("top", "18%");
+          seaweed2_3.style.setProperty("top", "26%");
+          island2.style.setProperty("bottom", "0");
+          islandwave2.style.setProperty("bottom", "0");
+          seaweedback2.style.setProperty("top", "15%");
+          text2.style.setProperty("opacity", "1");
+          for (let h of coral2) {
+            h.style.setProperty("bottom", "-1%");
+          }
+        }
 
-      //4
-      if(pos <= -1980){
-        
-        text3.style.setProperty('filter','blur(20px)')
-        fg.style.setProperty('filter', 'blur(50px)')
+        if (pos <= -500) {
+          for (let h of coral2) {
+            h.style.setProperty("--cb", 6 + pos * 0.005 + "px");
+          }
+          seaweedback2.style.setProperty("--cb", 5 + pos * 0.004 + "px");
+          seaweed1_2.style.setProperty("--cb", 5 + pos * 0.004 + "px");
+          seaweed2_2.style.setProperty("--cb", 6 + pos * 0.004 + "px");
+          seaweed2_3.style.setProperty("--cb", 4 + pos * 0.004 + "px");
+        }
       }
-      if(pos <= -2300){
-        rock4.style.setProperty('bottom', '-50%')
-        islandwave4.style.setProperty('bottom', '0%')
-        
+      if (pageid == "#page3") {
+        if (pos <= -1100) {
+          text2.style.setProperty("filter", "blur(20px)");
+        }
+
+        //3
+        if (pos <= -1200) {
+          sceneback3.style.setProperty("bottom", "15%");
+          scenemid3.style.setProperty("bottom", "0%");
+          scenefront3.style.setProperty("bottom", "0%");
+          text3.style.setProperty("margin-top", "15%");
+          text3.style.setProperty("opacity", "1");
+          shark3.style.setProperty("right", "-4%");
+          shark3.style.setProperty("top", "10%");
+        }
       }
-      if( pos <= -2600){
-        fg.style.setProperty('filter', 'blur(0px)')
-        
+      if (pageid == "#page4") {
+        if (pos <= -1700) {
+          // console.log(1)
+          if (pos >= -2000) {
+            cfront13.style.setProperty("--cbb", 10 + pos * 0.005 + "px");
+            cfront23.style.setProperty("--cbb", 10 + pos * 0.005 + "px");
+            cmid.style.setProperty("--cbb", 2 + pos * 0.005 + "px");
+          }
+        }
+
+        //4
+        if (pos <= -1980) {
+          text3.style.setProperty("filter", "blur(20px)");
+          fg.style.setProperty("filter", "blur(50px)");
+        }
+        if (pos <= -2300) {
+          rock4.style.setProperty("bottom", "-50%");
+          islandwave4.style.setProperty("bottom", "0%");
+        }
+        if (pos <= -2600) {
+          fg.style.setProperty("filter", "blur(0px)");
+        }
       }
       // ตัวกระตุกขาเข้า
-      // if (pos <= -2800){
-      //   // fg.style.setProperty('filter', 'blur(20px)')
-      //   text4.style.setProperty('filter','blur(20px)')
-      // }
+      if (pageid == "#page5") {
+        if (pos <= -2800) {
+          fg.style.setProperty("filter", "blur(20px)");
+          text4.style.setProperty("filter", "blur(20px)");
+        }
 
-      // 5
+        // 5
 
-      if(pos <= -3500){
-        fc1.style.setProperty('bottom', '18%')
-        fc2.style.setProperty('bottom', '18%')
-        fc3.style.setProperty('bottom', '18%')
+        if (pos <= -3500) {
+          fc1.style.setProperty("bottom", "18%");
+          fc2.style.setProperty("bottom", "18%");
+          fc3.style.setProperty("bottom", "18%");
+
+          // jame comment
+          // sp1.style.setProperty('left', '14.5%')
+          // sp3.style.setProperty('left', '64%')
+        }
+
+        if (pos <= -3750) {
+          rock5.style.setProperty("top", "-10%");
+        }
+      }
+
+      if (pageid == "#page6") {
+        // 6
+        // ตัวกระตุกขาออก
+        if (pos <= -3900) {
+          text5.style.setProperty("filter", "blur(20px)");
+        }
+        if (pos <= -4000) {
+          trash1.style.setProperty("top", "50%");
+          trash2.style.setProperty("top", "43%");
+          trash3.style.setProperty("top", "60%");
+
+          fc1.style.setProperty("bottom", "35%");
+          fc2.style.setProperty("bottom", "35%");
+          fc3.style.setProperty("bottom", "35%");
+        }
+
+        if (pos <= -4200) {
+          rockback6.style.setProperty("bottom", "18%");
+          rockmid6.style.setProperty("bottom", "0%");
+          cfront6.style.setProperty("bottom", "-3%");
+        }
 
         // jame comment
-        // sp1.style.setProperty('left', '14.5%')
-        // sp3.style.setProperty('left', '64%')
-       }
-      
-       if(pos <= -3750){
-        rock5.style.setProperty('top', '-10%')
-       }
-
-       //6
-       //ตัวกระตุกขาออก
-      //  if (pos <= -3900){
-      //   text5.style.setProperty('filter','blur(20px)')
-      //  }
-       if (pos <= -4000){
-        trash1.style.setProperty('top', '50%')
-        trash2.style.setProperty('top', '43%')
-        trash3.style.setProperty('top', '60%')
-
-        fc1.style.setProperty('bottom', '35%')
-        fc2.style.setProperty('bottom', '35%')
-        fc3.style.setProperty('bottom', '35%')
-        
-       }
-
-       if (pos <= -4200){
-        rockback6.style.setProperty('bottom', '18%')
-        rockmid6.style.setProperty('bottom', '0%')
-        cfront6.style.setProperty('bottom', '-3%')
-        
-       }
-
-      // jame comment
-      //  if (pos <= -4600){
-      //   trash1.style.setProperty('top', '50%')
-      //   trash2.style.setProperty('top', '43%')
-      //   trash3.style.setProperty('top', '60%')
-      //  }
-       
-
+        //  if (pos <= -4600){
+        //   trash1.style.setProperty('top', '50%')
+        //   trash2.style.setProperty('top', '43%')
+        //   trash3.style.setProperty('top', '60%')
+        //  }
+      }
     }
   }
 }
 
 // flip card page 5
 function flipCard(el) {
-  el.dataset.open *= -1
+  el.dataset.open *= -1;
 }
-
 
 // function setContent(){
 //   let point = document.querySelector('.head').getBoundingClientRect().top;
