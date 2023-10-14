a55dcd1a4bcba4ce07fad7d97f628df4b19d953b
Adamam <88430865+Adamam13@users.noreply.github.com>
2023-10-14 23:22:56 +0700

    test_object_colusion

--- GameFlappyFish/app.js
+++ GameFlappyFish/app.js
@@ -11,8 +11,14 @@ window.oncontextmenu = function(event) {
   return false;
 };
 
-let ob_top = document.querySelectorAll('.ob_top');
-let ob_bottom = document.querySelectorAll('.ob_bottom');
+function swim(){
+  player.style.setProperty('--poy', 0 + '%');
+}
+
+function down(){
+  player.style.setProperty('--poy', 70 + '%');
+}
+
 
 
 setInterval(game, 1);
@@ -24,13 +30,63 @@ console.log(p_l)
 let p_r = parseInt(player.getBoundingClientRect().right)
 console.log(p_r)
 
-function game(){
-  let p_t = parseInt(player.getBoundingClientRect().top)
-  // console.log(p_t)
-  let p_b = parseInt(player.getBoundingClientRect().bottom)
-  // console.log(p_b)
+function rock5(p_t, p_b){
+  let xs1 = 0
+  let ys1 = 0
 
-  //collision check
+  let xs2 = 52
+  let ys2 = 308
+
+  let xs3 = 256
+  let ys3 = 487
+
+  let ob_top = document.querySelectorAll('.rock5');
+
+  for (let t of ob_top){
+    let t_l = parseInt(t.getBoundingClientRect().left);
+    let t_r = parseInt(t.getBoundingClientRect().right);
+    let t_b = parseInt(t.getBoundingClientRect().bottom);
+    let t_t = parseInt(t.getBoundingClientRect().top);
+
+    let x1 = t_l + xs1;
+    let y1 = (ys1+t_t) * 0.6;
+
+    let x2 = t_l + xs2;
+    let y2 = (ys2+t_t) * 0.6;
+
+    let x3 = t_l + xs3;
+    let y3 = (ys3+t_t) * 0.6;
+
+    let m1 = (y2-y1) / (x2-x1);
+    let line1 = m1*(p_r-x1) + y1 - p_t;
+
+    let m2 = (y3-y2) / (x3-x2);
+    let line2 = m2*(p_r-x2) + y2 - p_t;
+
+
+    if (line1 >= 0 && p_t <= y2 && p_l <= t_r && p_t >= y1 && p_t <= t_b && p_r >= t_l){
+      console.log("yes")
+      console.log("line1", line1)
+      console.log("p_t", p_t)
+      console.log("y2", y2)
+      player.style.setProperty('--cor', 'red');
+    }
+
+    if (line2 >= 0 && p_t <= y3 && p_l <= t_r && p_t >= y2 && p_t <= t_b && p_r >= t_l){
+      console.log("yes")
+      console.log("line2", line2)
+      console.log("p_t", p_t)
+      console.log("y3", y3)
+      player.style.setProperty('--cor', 'red');
+    }
+  }
+
+}
+
+function rock(p_t){
+
+  let ob_top = document.querySelectorAll('.rock');
+
   for (let t of ob_top){
     let t_l = parseInt(t.getBoundingClientRect().left);
     let t_r = parseInt(t.getBoundingClientRect().right);
@@ -40,6 +96,12 @@ function game(){
         player.style.setProperty('--cor', 'red');
     }
   }
+
+}
+
+function coral(p_b){
+  
+  let ob_bottom = document.querySelectorAll('.coral');
 
   for (let b of ob_bottom){
     let b_l = parseInt(b.getBoundingClientRect().left);
@@ -52,10 +114,15 @@ function game(){
   }
 }
 
-function swim(){
-  player.style.setProperty('--poy', 0 + '%');
-}
 
-function down(){
-  player.style.setProperty('--poy', 70 + '%');
+function game(){
+  let p_t = parseInt(player.getBoundingClientRect().top)
+  // console.log(p_t)
+  let p_b = parseInt(player.getBoundingClientRect().bottom)
+  // console.log(p_b)
+  //collision check
+
+  rock5(p_t, p_b)
+  // rock(p_t)
+  coral(p_b)
 }
