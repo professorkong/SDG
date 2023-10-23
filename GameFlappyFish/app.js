b417c3c9153b07e751c0fb9dc8b4e1fe2dd35a5c
Adamam <88430865+Adamam13@users.noreply.github.com>
2023-10-23 08:52:55 +0700

    hit funtction and point funtion fix some bug that can see

--- GameFlappyFish/app.js
+++ GameFlappyFish/app.js
@@ -12,11 +12,15 @@ window.oncontextmenu = function(event) {
 };
 
 function swim(){
-  player.style.setProperty('--poy', 0 + '%');
+  if (isover == 0){
+    player.style.setProperty('--poy', 0 + '%');
+  }
 }
 
 function down(){
-  player.style.setProperty('--poy', 70 + '%');
+  if (isover == 0){
+    player.style.setProperty('--poy', 70 + '%');
+  }
 }
 
 
@@ -24,6 +28,14 @@ function down(){
 setInterval(game, 1);
 
 i = 0;
+let count = 0;
+let point = 0
+let time = 60;
+let ishit = 0;
+let fatic = 0;
+let map = 1;
+let isover = 0;
+
 
 let p_l = parseInt(player.getBoundingClientRect().left)
 console.log(p_l)
@@ -38,10 +50,12 @@ function rock(p_t){
     let t_l = parseInt(t.getBoundingClientRect().left);
     let t_r = parseInt(t.getBoundingClientRect().right);
     let t_b = parseInt(t.getBoundingClientRect().bottom);
-    if (p_r >= t_l && p_l <= t_r && p_t <= t_b){
+    if (p_r >= t_l && p_l <= t_r && p_t <= t_b && ishit == 0 && isover == 0){
       // console.log(t)
-      t.style.setProperty('--co', 'red');
+      // t.style.setProperty('--co', 'red');
       // player.style.setProperty('--cor', 'red');
+      ishit = 1;
+      knockback()
     }
   }
   
@@ -55,15 +69,75 @@ function coral(p_b){
     let b_l = parseInt(b.getBoundingClientRect().left);
     let b_r = parseInt(b.getBoundingClientRect().right);
     let b_t = parseInt(b.getBoundingClientRect().top);
-    if (p_r >= b_l && p_l <= b_r && p_b >= b_t){
-        // console.log(b)
-        b.style.setProperty('--co', 'red');
+    if (p_r >= b_l && p_l <= b_r && p_b >= b_t && ishit == 0 && isover == 0){
+        console.log(b)
+        // b.style.setProperty('--co', 'red');
         // player.style.setProperty('--cor', 'red');
+        ishit = 1;
+        knockback()
     }
   }
 }
+let op = 1;
 
-let count = 0;
+function knockback(){
+  let flex = document.querySelector(".flex");
+  let speed = '-1%';
+  if (point > 0){
+    point -= 100;
+    document.getElementById("point").innerHTML = point;
+  }
+  if (fatic == 0 && count >= 2.4){
+    count -= 1.2;
+    flex.style.setProperty('--count', count)
+    fatic = 1;
+  }
+  console.log("c = ",count)
+  flex.style.setProperty('--speed', speed)
+  temp = setInterval(kapib, 200)
+  setTimeout(res, 1000)
+}
+
+function res(){
+  clearInterval(temp)
+  ishit = 0;
+  let flex = document.querySelector(".flex");
+  let speed = "calc(-10000px * var(--count))";
+  flex.style.setProperty('--speed', speed);
+  op = 1;
+  player.style.setProperty('--op', op)
+
+  if (fatic == 1){
+      count += 1.2;
+      flex.style.setProperty('--count', count)
+      fatic = 0;
+  }
+
+  // let v = 0;
+  // setInterval(function(){
+  //   if (fatic == 1){
+  //     v += 1;
+  //     console.log('v',v);
+  //     if (v <= 12){
+  //       count += 0.1;
+  //       flex.style.setProperty('--count', count)
+  //       console.log("c = ", count);
+  //     }
+  //     else{
+  //       fatic = 0;
+  //       v = 0;
+  //     }
+  //   }
+  // }, 1000)
+
+}
+
+
+function kapib(){
+  op *= -1;
+  player.style.setProperty('--op', op)
+}
+
 
 function check(){
   let box = document.querySelectorAll(".background");
@@ -77,24 +151,26 @@ function check(){
       i.dataset.check = 1;
       let flex = document.querySelector(".flex");
       count += 1.2;
+      map += 1;
       flex.style.setProperty('--count', count)
       console.log("Check");
+      console.log(count);
     }
   }
 }
 
 function pass(){
-  let box = document.querySelectorAll(".box");
+  let box = document.querySelectorAll(".po");
   for (let i of box){
     let i_r = parseInt(i.getBoundingClientRect().right);
     if (i_r <= p_l && i.dataset.pass == 0){
       console.log("Pass");
       i.dataset.pass = 1;
+      point += 100;
+      document.getElementById("point").innerHTML = point;
     }
-
   }
 }
-
 
 
 function game(){
@@ -110,10 +186,6 @@ function game(){
   pass()
   check()
 }
-
-
-let point = 0
-let time = 60;
 
 document.getElementById('tutorial').style.visibility = 'visible';
 
@@ -138,16 +210,21 @@ function start() {
   setInterval(main, 1000);
   document.getElementById('tutorial').style.visibility = 'hidden';
   document.querySelector('.menu').style.visibility = 'visible';
-
+  
   let flex = document.querySelector(".flex");
   flex.style.setProperty('--count', 1.2)
+  console.log(count)
 }
 
 function gameOver(e) {
   clearInterval(e);
+  isover = 1;
+  count = 0;
   let flex = document.querySelector(".flex");
-  flex.style.setProperty('--count', 1.2)
-  flex.style.setProperty('--dif', '0s')
+  flex.style.setProperty('--count', 0)
+  flex.style.setProperty('--speed', "0")
+  flex.style.setProperty('--dif', '100000s')
+
 }
 
 // show character when end game is over 
@@ -172,7 +249,30 @@ function reset(){
   document.getElementById("content1").style.visibility = "hidden";
   document.getElementById("content2").style.visibility = "hidden";
   document.getElementById("content3").style.visibility = "hidden";
+
   let flex = document.querySelector(".flex");
-  flex.style.setProperty('--count', 1.2)
-  flex.style.setProperty('--dif', '40s')
+  flex.style.setProperty('--dif', 'none')
+  flex.style.setProperty('--count', 0)
+  flex.style.setProperty('--speed', "calc(-10000px * var(--count))")
+
+  let box = document.querySelectorAll(".po");
+  for (let i of box){
+    i.dataset.pass = 0;
+  }
+
+  let box2 = document.querySelectorAll(".background");
+  for (let i of box2){
+    i.dataset.check = 0;
+  }
+
+
+
+  setTimeout(function(){
+    time = 60;
+    isover = 0;
+    flex.style.setProperty('--count', 1.2)
+    flex.style.setProperty('--dif', '40s')
+  }, 1000)
+
+
 }
